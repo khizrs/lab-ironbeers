cd94f254f581b94fd41f88d3ebcf748bfa0315bd
Khizr Saharan <khizrs@gmail.com>
2021-02-05 10:45:45 +0100

    coded no css

--- app.js
+++ app.js
@@ -11,12 +11,38 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
 
 // Register the location for handlebars partials here:
 
 // ...
 
 // Add the route handlers here:
+app.get('/beers', async (req, res) => {
+  const beersData = await punkAPI.getBeers()
+
+  res.render('beers',{beersData});
+
+});
+
+app.get('/random-beer', async (req, res) => {
+  const randomBeerData = await punkAPI.getRandom()
+  console.log(randomBeerData)
+  res.render('random-beer',{randomBeerData});
+
+});
+
+app.get("/beers/:id", (req,res) => {
+  let id = req.params.id
+  console.log(id)
+  punkAPI
+  .getBeer(id)
+  .then(beer => {
+    res.render("beerinfo", {beer});
+  })
+ 
+})
 
 app.get('/', (req, res) => {
   res.render('index');
